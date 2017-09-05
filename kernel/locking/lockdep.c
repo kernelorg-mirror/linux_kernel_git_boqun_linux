ece5cb09840251c7cd5798c809bb2a6cf28c23c0
Boqun Feng <boqun.feng@gmail.com>
2017-09-05 20:20:30 +0800

    lockdep: Fix recursive read lock related safe->unsafe detection
    There are four cases for recursive read lock realted deadlocks:
    
    (--(X..Y)--> means a strong dependency path starts with a --(X*)-->
    dependency and ends with a --(*Y)-- dependency.)
    
    1.	An irq-safe lock L1 has a dependency --(*..*)--> to an
    	irq-unsafe lock L2.
    
    2.	An irq-read-safe lock L1 has a dependency --(N..*)--> to an
    	irq-unsafe lock L2.
    
    3.	An irq-safe lock L1 has a dependency --(*..N)--> to an
    	irq-read-unsafe lock L2.
    
    4.	An irq-read-safe lock L1 has a dependency --(N..N)--> to an
    	irq-read-unsafe lock L2.
    
    The current check_usage() only checks 1) and 2), so this patch adds
    checks for 3) and 4) and makes sure when find_usage_{back,for}wards find
    an irq-read-{,un}safe lock, the traverse path should ends at a
    dependency --(*N)-->. Note when we search backwards, --(*N)--> indicates
    a real dependency --(N*)-->.
    
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>

--- kernel/locking/lockdep.c
+++ kernel/locking/lockdep.c
@@ -1499,7 +1499,14 @@ check_redundant(struct lock_list *root, struct held_lock *target,
 
 static inline int usage_match(struct lock_list *entry, void *bit)
 {
-	return entry->class->usage_mask & (1 << (enum lock_usage_bit)bit);
+	enum lock_usage_bit ub = (enum lock_usage_bit)bit;
+
+
+	if (ub & 1)
+		return entry->class->usage_mask & (1 << ub) &&
+		       !entry->is_rr;
+	else
+		return entry->class->usage_mask & (1 << ub);
 }
 
 
@@ -1810,6 +1817,10 @@ static int check_irq_usage(struct task_struct *curr, struct held_lock *prev,
 			   exclusive_bit(bit), state_name(bit)))
 		return 0;
 
+	if (!check_usage(curr, prev, next, bit,
+			   exclusive_bit(bit) + 1, state_name(bit)))
+		return 0;
+
 	bit++; /* _READ */
 
 	/*
@@ -1820,6 +1831,10 @@ static int check_irq_usage(struct task_struct *curr, struct held_lock *prev,
 	 */
 	if (!check_usage(curr, prev, next, bit,
 			   exclusive_bit(bit), state_name(bit)))
+		return 0;
+
+	if (!check_usage(curr, prev, next, bit,
+			   exclusive_bit(bit) + 1, state_name(bit)))
 		return 0;
 
 	return 1;
