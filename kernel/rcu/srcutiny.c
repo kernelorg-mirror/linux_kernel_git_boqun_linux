4fa4c8e1e89a6088172085ee5374c95dd1cae1db
Boqun Feng <boqun.feng@gmail.com>
2018-04-16 14:39:18 +0800

    srcu: Make synchronize_srcu() of tiny SRCU a no-op if scheduler is inactive
    Similar to the synchronize_srcu() for tree SRCU, synchronize_srcu()
    could be a no-op if scheduler is not activated, because in that case,
    the kernel is running in single-threaded model, so there is no other
    thread running inside an SRCU read-side critical section when
    synchronize_srcu() is called, therefore no need to wait.
    
    Acked-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>

--- kernel/rcu/srcutiny.c
+++ kernel/rcu/srcutiny.c
@@ -188,6 +188,10 @@ void synchronize_srcu(struct srcu_struct *sp)
 {
 	struct rcu_synchronize rs;
 
+	/* this is a no-op if scheduler is inactive */
+	if (rcu_scheduler_active == RCU_SCHEDULER_INACTIVE)
+		return;
+
 	init_rcu_head_on_stack(&rs.head);
 	init_completion(&rs.completion);
 	call_srcu(sp, &rs.head, wakeme_after_rcu);
