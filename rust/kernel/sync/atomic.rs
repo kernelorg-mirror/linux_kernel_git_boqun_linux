d6bc3fdccfbb194cc4fb83d893a2fdd3b3c2301a
Boqun Feng <boqun.feng@gmail.com>
2025-06-18 09:49:28 -0700

    rust: sync: atomic: Add generic atomics
    To provide using LKMM atomics for Rust code, a generic `Atomic<T>` is
    added, currently `T` needs to be Send + Copy because these are the
    straightforward usages and all basic types support this.
    
    Implement `AtomicType` for `i32` and `i64`, and so far only basic
    operations load() and store() are introduced.
    
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>

--- rust/kernel/sync/atomic.rs
+++ rust/kernel/sync/atomic.rs
@@ -16,7 +16,22 @@
 //!
 //! [`LKMM`]: srctree/tools/memory-model/
 
+pub mod generic;
 pub mod ops;
 pub mod ordering;
 
+#[doc(inline)]
+pub use generic::Atomic;
 pub use ordering::{Acquire, Full, Relaxed, Release};
+
+// SAFETY: `i32` has the same size and alignment with itself, and is round-trip transmutable to
+// itself.
+unsafe impl generic::AtomicType for i32 {
+    type Repr = i32;
+}
+
+// SAFETY: `i64` has the same size and alignment with itself, and is round-trip transmutable to
+// itself.
+unsafe impl generic::AtomicType for i64 {
+    type Repr = i64;
+}
